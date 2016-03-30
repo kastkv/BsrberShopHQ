35a8ccadcfd37db192e4be821d2e122384662830
KV <kv@iMac-KV.local>
2016-03-30 00:34:23 -0700

    Update Gemfile

--- app.rb
+++ app.rb
@@ -1,4 +1,3 @@
-#encoding: utf-8
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader' # чтобы не перезапускать sinatra каждый раз
@@ -7,6 +6,3 @@ get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 
-get '/New' do   #добавляем обработчик
- erb "Hello World" # чтобы выводился с нашим шаблоном пишем erb
-end
